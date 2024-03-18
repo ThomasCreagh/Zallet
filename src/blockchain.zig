0c06afd60b6e1fe14e38c168f643a9d685a4f480
Thomas Creagh <44379222+ThomasCreagh@users.noreply.github.com>
2024-03-18 21:46:12 +0000

    added strings to struct

--- src/blockchain.zig
+++ src/blockchain.zig
@@ -1,12 +1,16 @@
+const std = @import("std");
 pub const Transaction = struct {
     amount: usize,
+    payer: []const u8,
+    payee: []const u8,
 };
 const Block = struct {
-
+    previous_hash: []u8,
+    transaction: Transaction,
+    const timestamp: i128 = std.time.nanoTimestamp();
+    pub fn hash(self: Block) u256 {
+        return std.crypto.hash.sha2.Sha256(self);
+    }
 };
-const Chain = struct {
-
-};
-const Wallet = struct {
-
-};
+const Chain = struct {};
+const Wallet = struct {};
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,12 @@
 const std = @import("std");
 const blockchain = @import("blockchain.zig");
 const print = std.debug.print;
+
 pub fn main() !void {
     const transaction = blockchain.Transaction {
         .amount = 100,
+        .payer = "Alice",
+        .payee = "Bob",
     };
-    print("{}", .{ transaction.amount });
+    print("{} {s} {s}", .{ transaction.amount, transaction.payer, transaction.payee });
 }
