6ca0c236d6d261e84de2d5ebab357a83e7954d91
Thomas Creagh <44379222+ThomasCreagh@users.noreply.github.com>
2024-03-19 00:11:07 +0000

    finally got struct as bytes

--- src/blockchain.zig
+++ src/blockchain.zig
@@ -1,16 +1,21 @@
 const std = @import("std");
+const sha2 = std.crypto.hash.sha2;
 pub const Transaction = struct {
-    amount: usize,
-    payer: []const u8,
-    payee: []const u8,
+    amount: u64,
+    payer: u256,
+    payee: u256,
 };
-const Block = struct {
-    previous_hash: []u8,
+pub const Block = struct {
+    previous_hash: ?[]u8,
     transaction: Transaction,
     const timestamp: i128 = std.time.nanoTimestamp();
-    pub fn hash(self: Block) u256 {
-        return std.crypto.hash.sha2.Sha256(self);
+    pub fn hash(self: Block) [32]u8 {
+        var sha256State = sha2.Sha256.init(sha2.Sha256.Options{});
+        sha256State.update(std.mem.asBytes(&self));
+        var digest: [32]u8 = undefined;
+        sha256State.final(&digest);
+        return digest;
     }
 };
-const Chain = struct {};
-const Wallet = struct {};
+pub const Chain = struct {};
+pub const Wallet = struct {};
--- src/main.zig
+++ src/main.zig
@@ -5,8 +5,13 @@ const print = std.debug.print;
 pub fn main() !void {
     const transaction = blockchain.Transaction {
         .amount = 100,
-        .payer = "Alice",
-        .payee = "Bob",
+        .payer = 0,
+        .payee = 1,
     };
-    print("{} {s} {s}", .{ transaction.amount, transaction.payer, transaction.payee });
+    const block = blockchain.Block {
+        .previous_hash = null,
+        .transaction = transaction,
+    };
+
+    print("{s}", .{ block.hash() });
 }
